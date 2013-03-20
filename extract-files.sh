b4b2b3788bd8ef87bce536d47382e6eda12f6508
jt1134 <jt1134@gmail.com>
2013-03-19 21:58:27 -0500

    mesmerizemtd: Cleanup
    - remove unnecessary makefiles and scripts
    - call extract-files.sh from fascinatemtd tree, as we're using the
      same proprietary files for both devices

--- extract-files.sh
+++ extract-files.sh
@@ -1,17 +1,3 @@
 #!/bin/sh
-
-VENDOR=samsung
-DEVICE=mesmerizemtd
-
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
-
-for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
-    adb pull /system/$FILE $BASE/$FILE
-done
-
-./setup-makefiles.sh
+# We're using the same proprietary files as the fascinate
+../fascinatemtd/extract-files.sh
